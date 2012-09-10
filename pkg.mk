dcff4e25ce3ba44d7dc17668ddce21001577cdfd
fred <fred@fred-desktop.(none)>
2012-09-10 19:01:58 +0200

    correct description in rouge.ji3c

--- pkg.mk
+++ pkg.mk
@@ -2,7 +2,7 @@
 WIN32_COMPONENTS = DLL ji3.bat Data Couleurs LICENSE README.md TestNormal
 SRC_COMPONENTS = ji3.bat src Makefile ji3.pro Data Couleurs LICENSE README.md TestNormal 
 
-pkg: pkg-win32 pkg-src pkg-other
+pkg: pkg-win32 pkg-src
 
 
 WIN_PKG_DIR = ji3-win32
@@ -12,13 +12,13 @@ pkg-win32:
 	mkdir -p $(WIN_PKG_DIR)
 	cp -f win32/ji3.exe DLL/.
 	for c in $(WIN32_COMPONENTS); do ln -fs ../$$c $(WIN_PKG_DIR)/$$c; done
-	zip -r $(WIN_PKG_DIR).zip $(WIN_PKG_DIR)
+	zip -r $(WIN_PKG_DIR).zip $(WIN_PKG_DIR) --exclude "*.log" --exclude "*.ji3u"
 
 pkg-src:
 	rm -rf $(SRC_PKG_DIR)
 	mkdir -p $(SRC_PKG_DIR)
 	for c in $(SRC_COMPONENTS); do ln -fs ../$$c $(SRC_PKG_DIR)/$$c; done
-	zip -r $(SRC_PKG_DIR).zip $(SRC_PKG_DIR)
+	zip -r $(SRC_PKG_DIR).zip $(SRC_PKG_DIR) --exclude "*.log" --exclude "*.ji3u"
 
 pkg-other:
 	zip -r ji3-Instruments.zip Instruments
